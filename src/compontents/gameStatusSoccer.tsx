5bb5ade22b0efada569ceb15b184752d019b728b
Osmair <osmair144@gmail.com>
2024-09-27 13:46:09 -0300

    up

--- src/compontents/gameStatusSoccer.tsx
+++ src/compontents/gameStatusSoccer.tsx
@@ -20,12 +20,26 @@ export function GameStatusSoccer({ playersOne, playersTwo }: Props) {
   const [teamData, setTeamData] = useState<PropsTeam[]>([])
   const [nameTeamOne, setNameTeamOne] = useState('')
   const [nameTeamTwo, setNameTeamTwo] = useState('')
+  const [goalOne, setGoalOne] = useState(0)
+  const [yellowCardOne, setYellowCardOne] = useState(0)
+  const [redCardOne, setRedCardOne] = useState(0)
+  const [faultOne, setFaultOne] = useState(0)
+  const [defeatOne, setDefeatOne] = useState(0)
+  const [goalTwo, setGoalTwo] = useState(0)
+  const [yellowCardTwo, setYellowCardTwo] = useState(0)
+  const [redCardTwo, setRedCardTwo] = useState(0)
+  const [faultTwo, setFaultTwo] = useState(0)
+  const [defeatTwo, setDefeatTwo] = useState(0)
 
   async function loadTeamName(id: number) {
     const dataTeam = await supabase.from('teams').select('name').eq('id', id)
     if(dataTeam.data) {
       return dataTeam.data[0].name
     }
+  }
+
+  function changeStatus(id: number, status: number, setStatus: (status: number)=>void) {
+    setStatus(status + 1)
   }
 
   useEffect(() => {
@@ -47,21 +61,32 @@ export function GameStatusSoccer({ playersOne, playersTwo }: Props) {
             { playersOne.map(item => (
             <View key={item.id} style={estatistica.playerGroup}>
               <Text style={estatistica.player}>{item.name}</Text>
-              <TouchableOpacity style={estatistica.itemPlayer}>
+
+              <TouchableOpacity style={estatistica.itemPlayer} onPress={() => changeStatus(item.id, faultOne, setFaultOne)}>
+                <Text>{faultOne}</Text>
                 <FontAwesome name="unlink" color="orange" size={24} />
               </TouchableOpacity>
+
               <TouchableOpacity style={estatistica.itemPlayer}>
+                <Text>{yellowCardOne}</Text>
                 <FontAwesome name="square" color="yellow" size={24} />
               </TouchableOpacity>
+
               <TouchableOpacity style={estatistica.itemPlayer}>
-              <FontAwesome name="square" color="red" size={24} />
+                <Text>{redCardOne}</Text>
+                <FontAwesome name="square" color="red" size={24} />
               </TouchableOpacity>
+
               <TouchableOpacity style={estatistica.itemPlayer}>
+                <Text>{goalOne}</Text>
                 <FontAwesome name="soccer-ball-o" color="blue" size={24} />
               </TouchableOpacity>
+
               <TouchableOpacity style={estatistica.itemPlayer}>
+                <Text>{defeatOne}</Text>
                 <FontAwesome name="hand-stop-o" color="green" size={24} />
               </TouchableOpacity>
+
             </View>
             ))
           }
@@ -74,21 +99,32 @@ export function GameStatusSoccer({ playersOne, playersTwo }: Props) {
             { playersTwo.map(item => (
             <View key={item.id} style={estatistica.playerGroup}>
               <Text style={estatistica.player}>{item.name}</Text>
+
               <TouchableOpacity style={estatistica.itemPlayer}>
+                <Text>{faultTwo}</Text>
                 <FontAwesome name="unlink" color="orange" size={24} />
               </TouchableOpacity>
+
               <TouchableOpacity style={estatistica.itemPlayer}>
+                <Text>{yellowCardTwo}</Text>
                 <FontAwesome name="square" color="yellow" size={24} />
               </TouchableOpacity>
+
               <TouchableOpacity style={estatistica.itemPlayer}>
-              <FontAwesome name="square" color="red" size={24} />
+                <Text>{redCardTwo}</Text>
+                <FontAwesome name="square" color="red" size={24} />
               </TouchableOpacity>
+
               <TouchableOpacity style={estatistica.itemPlayer}>
+                <Text>{goalTwo}</Text>
                 <FontAwesome name="soccer-ball-o" color="blue" size={24} />
               </TouchableOpacity>
+
               <TouchableOpacity style={estatistica.itemPlayer}>
+                <Text>{defeatTwo}</Text>
                 <FontAwesome name="hand-stop-o" color="green" size={24} />
               </TouchableOpacity>
+
             </View>
             ))
           }
